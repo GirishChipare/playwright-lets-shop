d11415e8f5f2c0c91afa48d91412ddbbd9e12ebd
Girish Chipare <91129881+GirishChipare@users.noreply.github.com>
2025-05-21 13:52:15 +0530

    adding few testcases

--- tests/Homepage.spec.js
+++ tests/Homepage.spec.js
@@ -1,7 +1,10 @@
 const {test,expect}   = require ('@playwright/test');
 const {APIUtils} = require('../utils/APIUtils');
+const { title } = require('process');
+const { match } = require('assert');
 
-test.only('Verify homepage title and metadata', async ({page}) => {
+
+test('Verify homepage title and metadata', async ({page}) => {
     await page.goto("https://rahulshettyacademy.com/client/");
     expect(page).toHaveTitle("Let's Shop");  
 
@@ -12,4 +15,30 @@ test.only('Verify homepage title and metadata', async ({page}) => {
     await expect(metaViewport).toHaveAttribute('content', 'width=device-width, initial-scale=1'); 
     await expect(divStyle).toHaveAttribute('style', 'background-color: white;'); 
 });
+
+test('Login and Logo check', async ({page}) => {
+    const email = "motanshikaa@gmail.com";
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.locator("#userEmail").fill(email);
+    await page.locator("#userPassword").fill("Anshika@123");
+    await page.locator("[value='Login']").click();
+    await page.waitForLoadState('networkidle');
+    expect(await page.locator('.logo').isVisible());
+});
+
+test('Search bar functionality', async({page}) =>{
+    const email = "motanshikaa@gmail.com";
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.locator("#userEmail").fill(email);
+    await page.locator("#userPassword").fill("Anshika@123");
+    await page.locator("[value='Login']").click();
+    await page.waitForLoadState('networkidle');
+    await page.locator("section [name='search']").fill("ZARA");
+    await page.locator('#products').click();
+    await page.waitForLoadState('domcontentloaded');
+    const productTitles = await page.locator("#products h5").allTextContents();
+    console.log(productTitles);
+    const matched = productTitles.some(title => title.includes('ZARA'));
+    expect(match).toBeTruthy();
+})
 
